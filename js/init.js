76d805afbbbcfb17f2871bafa7c4a40f412feacb
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-08-24 11:01:55 -0400

    Remove log

--- js/init.js
+++ js/init.js
@@ -2,8 +2,6 @@ function updateCanvasSize() {
   var canvas = document.getElementById("gol");
   canvas.setAttribute("width", canvas.offsetWidth);
   canvas.setAttribute("height", canvas.offsetHeight);
-
-  console.log("Size:", canvas.getAttribute("width"), canvas.getAttribute("height"));
 }
 
 document.addEventListener("DOMContentLoaded", updateCanvasSize);
