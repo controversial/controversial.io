eebef3971e17a9a923fcf45dd039954cb50bb4b5
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-08-28 12:27:45 -0400

    Remove click event from down arrow

--- js/init.js
+++ js/init.js
@@ -4,12 +4,7 @@ var ctrl = new ScrollMagic.Controller();
 
 ctrl.scrollTo(function (newpos) {
   TweenLite.to(window, 1, {
-    scrollTo: {
-      y: newpos
-    },
-    css: {
-      "pointer-events": "none"
-    }
+    scrollTo: {y: newpos}
   });
 });
 
@@ -22,28 +17,10 @@ var scenes = [
 
   new ScrollMagic.Scene({
     duration: "50%",
-    offset: 10
-  }).setTween(".down-indicator", {
-      opacity: 0,
-    }).on("enter", function(e) {
-      document.getElementsByClassName("down-indicator")[0].style["pointer-events"] = "none";
-    }).on("leave", function(e) {
-      document.getElementsByClassName("down-indicator")[0].style["pointer-events"] = e.scrollDirection === "REVERSE" ? "inherit": "none";
-    })
-
+  }).setTween(".down-indicator", {opacity: 0})
 ];
 
 // Add scenes to the controller
 for (var i=0; i<scenes.length; i++) {
   scenes[i] = scenes[i].addTo(ctrl);
-}
-
-
-// Bind down indicators to scroll down function
-var downIndicators = document.querySelectorAll(".down-indicator > i");
-function sectionJump () {
-  ctrl.scrollTo(window.scrollY + window.innerWidth);
-}
-for (var i = 0; i < downIndicators.length; i++) {
-  downIndicators[i].onclick = sectionJump;
 }
