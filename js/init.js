a0a1f0277d5f13d3da25a00151041c1706dd6308
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-08-28 19:06:17 -0400

    Kill ScrollMagic/GSAP, vanilla ftw

--- js/init.js
+++ js/init.js
@@ -1,50 +1,43 @@
-// Set up ScrollMagic stuff
+var backgroundScale = new chroma.scale(["#2d3940", "#fff"]).mode("lab");
 
-var ctrl = new ScrollMagic.Controller();
+document.addEventListener("scroll", function(e) {
+  var scroll = window.scrollY;
 
-ctrl.scrollTo(function (newpos) {
-  TweenLite.to(window, 1, {
-    scrollTo: {y: newpos}
-  });
+  /****** STAGE 1 ******
+   * HEADER SHRINK AND *
+   * DOWN ICON OPACITY *
+   *********************/
+  var header = document.getElementsByTagName("header")[0];
+  var headerTitle = document.getElementsByTagName("h1")[0];
+  var downIndicator = document.getElementsByClassName("down-indicator")[0];
+
+  var headerProgress = scroll / (window.innerHeight / 2);
+  // Starting phase
+  if (scroll < 5) {
+    header.style.width = "100vw";
+    header.style.height = "100vh";
+    downIndicator.style.opacity = 1;
+    document.body.style.backgroundColor = backgroundScale(0).hex();
+  // Intermediate phases
+  } else if (scroll < window.innerHeight / 2) {
+    header.style.top = "50vh";
+    headerTitle.style.top = "50vh";
+    header.style.position = "fixed";
+    headerTitle.style.position = "fixed";
+
+    header.style.width = ((1 - headerProgress) * 50 + 50).toString() + "vw";
+    header.style.height = ((1 - headerProgress) * 50 + 50).toString() + "vh";
+    downIndicator.style.opacity = 1 - headerProgress;
+    document.body.style.backgroundColor = backgroundScale(headerProgress).hex();
+  // End phase
+  } else {
+    header.style.top = window.innerHeight + "px";
+    headerTitle.style.top = window.innerHeight + "px";
+    header.style.position = "absolute";
+    headerTitle.style.position = "absolute";
+    header.style.width = "50vw";
+    header.style.height = "50vh";
+    downIndicator.style.opacity = 0;
+    document.body.style.backgroundColor = backgroundScale(1).hex();
+  }
 });
-
-
-// Set up ScrollMagic scenes
-var scenes = [
-
-
-  // Fade down button
-
-  new ScrollMagic.Scene({
-    duration: "50%",
-  }).setTween(".down-indicator", {opacity: 0}),
-
-
-  // Shrink header
-
-  new ScrollMagic.Scene({
-    duration: "50%"
-  }).setTween("header", {
-    css: {
-      width: window.innerWidth / 2,
-      height: window.innerHeight / 2,
-      left: window.innerWidth / 4,
-      top: window.innerHeight / 4
-    }
-  }).setPin("header"),
-
-
-  // Fade background to white
-  new ScrollMagic.Scene({
-    duration: "50%"
-  }).setTween("body", {
-    css: {
-      backgroundColor: "#fff"
-    }
-  })
-];
-
-// Add scenes to the controller
-for (var i=0; i<scenes.length; i++) {
-  scenes[i] = scenes[i].addTo(ctrl);
-}
