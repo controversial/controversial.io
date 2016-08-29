7e2c293e369cd56243538551c4b7078d042e381e
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-08-29 14:34:52 -0400

    (hopefully) improve performance by moving static things outside of the loop

--- js/init.js
+++ js/init.js
@@ -1,5 +1,55 @@
 var backgroundScale = new chroma.scale(["#2d3940", "#fff"]).mode("lab");
 
+var elem = {
+  header: document.getElementsByTagName("header")[0],
+  headerTitle: document.getElementsByTagName("h1")[0],
+  laptop: document.getElementsByClassName("laptop")[0],
+  downIndicator: document.getElementsByClassName("down-indicator")[0],
+  laptopContent: document.getElementsByClassName("laptop-content")[0],
+  body: document.body
+};
+
+function setHeaderElementsPinned(fixed) {
+  var elements = [elem.headerTitle, elem.laptop];
+  for (var i=0; i<elements.length; i++) {
+    elements[i].style.position = fixed ? "fixed" : "absolute";
+    elements[i].style.top = fixed ? "50vh" : window.innerHeight + "px";
+  }
+}
+
+function updateHeaderElements(progress) {
+
+  function tween(a, b) {
+    return a + (b - a) * progress;
+  }
+
+  elem.header.style.width = tween(
+    100,
+    (100 * elem.laptopContent.offsetWidth / window.innerWidth)
+  ) + "vw";
+  elem.header.style.height = tween(
+    100,
+    (100 * elem.laptopContent.offsetHeight / window.innerHeight)
+  ) + "vh";
+
+  elem.header.style.top = tween(
+    window.innerHeight / 2,  // 50vh
+    elem.laptopContent.offsetTop + elem.laptopContent.offsetHeight
+  ) + "px";
+
+  if (progress === 1) {
+    elem.header.style.top = elem.header.offsetTop + window.innerHeight / 2 + "px";
+    elem.header.style.position = "absolute";
+  } else {
+    elem.header.style.position = "fixed";
+  }
+  elem.downIndicator.style.opacity = 1 - progress;
+  elem.body.style.backgroundColor = backgroundScale(progress).hex();
+
+  // TODO: Update canvas pixel size
+}
+
+
 document.addEventListener("scroll", function(e) {
   var scroll = window.scrollY;
 
@@ -7,51 +57,8 @@ document.addEventListener("scroll", function(e) {
    * HEADER SHRINK AND *
    * DOWN ICON OPACITY *
    *********************/
-  var header = document.getElementsByTagName("header")[0];
-  var headerTitle = document.getElementsByTagName("h1")[0];
-  var laptop = document.getElementsByClassName("laptop")[0];
-  var downIndicator = document.getElementsByClassName("down-indicator")[0];
-  var laptopContent = laptop.getElementsByClassName("laptop-content")[0];
 
   var headerProgress = scroll / (window.innerHeight / 2);
-
-  function setHeaderElementsPinned(fixed) {
-    elements = [headerTitle, laptop];
-    for (var i=0; i<elements.length; i++) {
-      elements[i].style.position = fixed ? "fixed" : "absolute";
-      elements[i].style.top = fixed ? "50vh" : window.innerHeight + "px";
-    }
-  }
-
-  function updateHeaderElements(progress) {
-
-    function tween(a, b) {
-      return a + (b - a) * progress;
-    }
-
-    header.style.width = tween(
-      100,
-      (100 * laptopContent.offsetWidth / window.innerWidth)
-    ) + "vw";
-    header.style.height = tween(
-      100,
-      (100 * laptopContent.offsetHeight / window.innerHeight)
-    ) + "vh";
-    header.style.top = tween(
-      window.innerHeight / 2,  // 50vh
-      laptopContent.offsetTop + laptopContent.offsetHeight
-    ) + "px";
-    if (progress === 1) {
-      header.style.top = header.offsetTop + window.innerHeight / 2 + "px";
-      header.style.position = "absolute";
-    } else {
-      header.style.position = "fixed";
-    }
-    downIndicator.style.opacity = 1 - progress;
-    document.body.style.backgroundColor = backgroundScale(headerProgress).hex();
-
-    // TODO: Update canvas pixel size
-  }
 
   switch(true) {
     case (scroll < 5):
