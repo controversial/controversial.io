233794d916b76220ffd8d717b15be80faf7f3637
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-09-07 21:22:02 -0400

    Compile sass with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+
 
 gulp.task("default", function() {
-
+  gulp.src("./sass/**/*.sass")
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: "compressed"}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./css"));
 });
