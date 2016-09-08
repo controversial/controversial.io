8445276c0c24925f8d4ab6817d0813aec0049aa6
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-09-07 21:26:44 -0400

    Rename gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
 
 
-gulp.task("default", function() {
+gulp.task("sass", function() {
   gulp.src("./sass/**/*.sass")
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: "compressed"}))
