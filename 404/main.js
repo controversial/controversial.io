8480cd8ed1d8a0ad263d924c423d8fc145c7048a
Luke Deen Taylor <luke@deentaylor.com>
2016-10-16 22:55:11 -0400

    Make 404/main.js airbnb-compliant

--- 404/main.js
+++ 404/main.js
@@ -1,27 +1,32 @@
 // 404 page using mapbox to show cities around the world.
 
 // Helper to generate the kind of coordinate pairs I'm using to store cities
-function coords() {
-  var center = map.getCenter();
-  return {lat: center.lat, lng: center.lng, zoom: map.getZoom()};
-}
 
+/* global L, scenes */ // These come from other scripts
 
-L.mapbox.accessToken = "pk.eyJ1IjoiY29udHJvdmVyc2lhbCIsImEiOiJjaXMwaXEwYjUwM2l6MnpwOHdodTh6Y24xIn0.JOD0uX_n_KXqhJ7ERnK0Lg";
-var map = L.mapbox.map("map", "mapbox.pencil", {
+L.mapbox.accessToken = 'pk.eyJ1IjoiY29udHJvdmVyc2lhbCIsImEiOiJjaXMwaXEwYjUwM2l6MnpwOHdodTh6Y24xIn0.JOD0uX_n_KXqhJ7ERnK0Lg';
+const map = L.mapbox.map('map', 'mapbox.pencil', {
   // Disable zooming by all methods
   zoomControl: false,
   touchZoom: false,
   scrollWheelZoom: false,
   doubleClickZoom: false,
-  boxZoom: false
+  boxZoom: false,
 });
 
+function coords() {
+  const center = map.getCenter();
+  return { lat: center.lat, lng: center.lng, zoom: map.getZoom() };
+}
+
 function go(area) {
-  var areas = Object.keys(scenes);
-  area = area || areas[Math.floor(Math.random() * areas.length)];
-  var scene = scenes[area];
-  console.log("Permalink here: " + "https://controversial.io/404?" + area);
+  const areas = Object.keys(scenes);
+  const place = area || areas[Math.floor(Math.random() * areas.length)];
+  const scene = scenes[place];
+  /* eslint-disable no-console */
+  console.log(`Permalink here: https://controversial.io/404?${area}`);
+  console.log(`You're at ${coords()}`);
+  /* eslint-enable no-console */
   map.setView(
     [scene.lat, scene.lng],
     scene.zoom
