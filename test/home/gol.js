26a55b505793c022dccbcfdc8120c837d24e1f16
Luke Deen Taylor <luke@deentaylor.com>
2016-10-23 23:00:15 -0400

    Fill in missing canvas functions

--- test/home/gol.js
+++ test/home/gol.js
@@ -6,18 +6,28 @@ const jsdom = require('jsdom').jsdom;
 const mainScript = require.resolve('../../app/js/home/gol.js');
 
 
-// TESTS ======================================================================
+// SET UP JSDOM ===============================================================
 
 const document = jsdom(`
 <html>
   <body>
+    <!-- Simulation will be spawned on this canvas -->
     <canvas id="gol"></canvas>
+
+    <!-- The Game of Life script -->
     <script src="${mainScript}">
   </body>
 </html>
 `);
 const window = document.defaultView;
+// Patch canvas.getContext() with fake canvas functions
+window.HTMLCanvasElement.prototype.getContext = () => ({
+  clearRect: () => {},
+  fillRect: () => {},
+});
 
+
+// TESTS ======================================================================
 
 test('properly initializes game of life', (t) => {
   t.true(typeof window.gol === 'object');
