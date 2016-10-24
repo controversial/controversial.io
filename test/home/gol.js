e4db3d831d4cf49a73cf79f4cf0ac037812a799a
Luke Deen Taylor <luke@deentaylor.com>
2016-10-23 23:01:00 -0400

    Forward console output from jsdom to node console

--- test/home/gol.js
+++ test/home/gol.js
@@ -1,14 +1,14 @@
 // Test the game of life script
 
 const test = require('ava');
-const jsdom = require('jsdom').jsdom;
+const jsdom = require('jsdom');
 
 const mainScript = require.resolve('../../app/js/home/gol.js');
 
 
 // SET UP JSDOM ===============================================================
 
-const document = jsdom(`
+const document = jsdom.jsdom(`
 <html>
   <body>
     <!-- Simulation will be spawned on this canvas -->
@@ -18,7 +18,9 @@ const document = jsdom(`
     <script src="${mainScript}">
   </body>
 </html>
-`);
+`, {
+  virtualConsole: jsdom.createVirtualConsole().sendTo(console),
+});
 const window = document.defaultView;
 // Patch canvas.getContext() with fake canvas functions
 window.HTMLCanvasElement.prototype.getContext = () => ({
