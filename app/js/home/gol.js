bda831c7376686dc6a7d37cec6e5f552e6595ab0
Luke Deen Taylor <luke@deentaylor.com>
2016-10-27 21:53:22 -0400

    Use Game class for the actual game of life thing

--- app/js/home/gol.js
+++ app/js/home/gol.js
@@ -192,21 +192,7 @@ document.addEventListener('DOMContentLoaded', () => {
     idealCellSize: 20,
     cellSize: [20, 20], // This gets updated dynamically.
 
-    boardSize: [10, 10],
-
-    board: undefined,
-
-    // HELPER FUNCTIONS =======================================================
-
-    getBlankBoard() {
-      return new Array(this.boardSize[0]).fill(0).map(
-        () => new Array(this.boardSize[1]).fill(false)
-      );
-    },
-
-    getRandomRow() {
-      return new Array(window.gol.boardSize[0]).fill(0).map(() => Math.random() < 0.125);
-    },
+    game: new Game(),
 
     // RENDERING FUNCTIONS ====================================================
     // These functions connect the simulation to the browser
@@ -214,9 +200,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // Called to update the game if the canvas size
     // changes.
     sizeChanged() {
-      // Spare loop variables
-      let i;
-
       const width = window.gol.canvas.offsetWidth;
       const height = window.gol.canvas.offsetHeight;
 
@@ -237,48 +220,13 @@ document.addEventListener('DOMContentLoaded', () => {
       // When board size changes, kill cells that are
       // cut off and randomize new terrain.
 
-      const oldBoardSize = window.gol.boardSize;  // Store old size
-      window.gol.boardSize = [  // Adjust size
+      window.gol.game.changeSize(
         Math.round(width / window.gol.cellSize[0]),
         Math.round(height / window.gol.cellSize[1]),
-      ];
-      const diff = [  // Calculate size difference
-        window.gol.boardSize[0] - oldBoardSize[0],
-        window.gol.boardSize[1] - oldBoardSize[1],
-      ];
-
-      // Kill dead cells
-
-      if (diff[0] < 0) {
-        window.gol.board = window.gol.board.slice(0, window.gol.boardSize[0]);
-      }
-      if (diff[1] < 0) {
-        window.gol.board = window.gol.board.map(row => row.slice(0, window.gol.boardSize[1]));
-      }
-
-      // Populate new cells
-
-      // Add new rows filled with random cells
-      for (i = 0; i < diff[0]; i += 1) {
-        window.gol.board.push(window.gol.getRandomRow());
-      }
-      // Add new random cells to existing rows (new columns)
-      for (i = 0; i < diff[1]; i += 1) {
-        for (let k = 0; k < window.gol.board.length; k += 1) {
-          window.gol.board[k].push(Math.random() < 0.125);
-        }
-      }
+      );
 
       window.gol.redraw();
     },
-
-    // Randomize the game state
-
-    randomize() {
-      this.board = this.getBlankBoard().map(() => this.getRandomRow());
-    },
-
-    // Render the board on the canvas
 
     redraw() {
       const width = this.canvas.getAttribute('width');
@@ -287,9 +235,9 @@ document.addEventListener('DOMContentLoaded', () => {
       this.ctx.clearRect(0, 0, width, height);
       this.ctx.fillStyle = this.cellColor;
       // Render board
-      for (let x = 0; x < this.boardSize[0]; x += 1) {
-        for (let y = 0; y < this.boardSize[1]; y += 1) {
-          if (this.board[x][y]) {
+      for (let x = 0; x < this.game.board.length; x += 1) {
+        for (let y = 0; y < this.game.board[0].length; y += 1) {
+          if (this.game.board[x][y]) {
             this.ctx.fillRect(
               this.cellSize[0] * x,
               this.cellSize[1] * y,
@@ -301,68 +249,6 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     },
 
-    // GAME LOGIC =============================================================
-
-    countLiveNeighbors(x, y) {
-      // Is there room in directions of:
-      //   - negative x
-      //   - negative y
-      //   - positive x
-      //   - positive y
-      const xroomneg = x > 0;
-      const yroomneg = y > 0;
-      const xroompos = x < this.boardSize[0] - 1;
-      const yroompos = y < this.boardSize[1] - 1;
-
-      const neighbors = [
-        // above
-        yroomneg && this.board[x][y - 1],
-        // above right
-        xroompos && yroomneg && this.board[x + 1][y - 1],
-        // right
-        xroompos && this.board[x + 1][y],
-        // below right
-        xroompos && yroompos && this.board[x + 1][y + 1],
-        // below
-        yroompos && this.board[x][y + 1],
-        // below left
-        xroomneg && yroompos && this.board[x - 1][y + 1],
-        // left
-        xroomneg && this.board[x - 1][y],
-        // above and left
-        xroomneg && yroompos && this.board[x - 1][y - 1],
-      ];
-
-      // Number of true values in neighbors array
-      return neighbors.filter(n => n).length;
-    },
-
-    judgeFate(isLive, liveNeighbors) {
-      if (isLive) {
-        // Any live cell with fewer than 2 live neighbors
-        // dies of underpopulation; any live cell with more
-        // than three dies of overpopulation.
-        return (liveNeighbors === 2 || liveNeighbors === 3);
-      }
-
-      // Any dead cell with exactly 3 neighbors is born
-      return liveNeighbors === 3;
-    },
-
-    step() {
-      const newState = this.getBlankBoard();
-
-      for (let x = 0; x < this.boardSize[0]; x += 1) {
-        for (let y = 0; y < this.boardSize[1]; y += 1) {
-          newState[x][y] = this.judgeFate(
-            this.board[x][y],
-            this.countLiveNeighbors(x, y)
-          );
-        }
-      }
-
-      this.board = newState;
-    },
 
     // USER INTERACTION =======================================================
 
@@ -389,7 +275,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const cellx = Math.floor(relx / window.gol.cellSize[0]);
         const celly = Math.floor(rely / window.gol.cellSize[1]);
 
-        window.gol.board[cellx][celly] = true;
+        window.gol.game.turnOn(cellx, celly);
       }
     },
 
@@ -398,7 +284,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Begin the loop
 
     start() {
-      window.gol.step();
+      window.gol.game.step();
       window.gol.redraw();
       setTimeout(() => {
         requestAnimationFrame(window.gol.start);
@@ -408,13 +294,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initialize and begin
 
     init() {
-      // Temporary board
-      this.board = this.getBlankBoard();
-      // Calculate sizes
       this.sizeChanged();
-      // Initialize board
-      this.board = this.getBlankBoard();
-      this.randomize();
+      this.game.randomize();
       this.start();
     },
   };
