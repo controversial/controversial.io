a6feb27c75a9b5d5cc3796c5668c495ee821d167
Luke Deen Taylor <luke@deentaylor.com>
2016-10-29 20:09:41 -0400

    Begin tests for real this time

--- test/home/gol.js
+++ test/home/gol.js
@@ -1,36 +1,9 @@
 // Test the game of life script
 
 const test = require('ava');
-const jsdom = require('jsdom');
-
-const mainScript = require.resolve('../../app/js/home/gol.js');
-
-
-// SET UP JSDOM ===============================================================
-
-const document = jsdom.jsdom(`
-<html>
-  <body>
-    <!-- Simulation will be spawned on this canvas -->
-    <canvas id="gol"></canvas>
-
-    <!-- The Game of Life script -->
-    <script src="${mainScript}">
-  </body>
-</html>
-`, {
-  virtualConsole: jsdom.createVirtualConsole().sendTo(console),
-});
-const window = document.defaultView;
-// Patch canvas.getContext() with fake canvas functions
-window.HTMLCanvasElement.prototype.getContext = () => ({
-  clearRect: () => {},
-  fillRect: () => {},
-});
-
-
-// TESTS ======================================================================
+const gol = require('../../app/js/home/gol.js');
 
 test('properly initializes game of life', (t) => {
-  t.true(typeof window.gol === 'object');
+  const g = new gol.Game();
+  t.true(g instanceof gol.Game);
 });
