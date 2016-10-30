08caa55f54439b50ef778f33f356e8e47a93d6df
Luke Deen Taylor <luke@deentaylor.com>
2016-10-29 23:26:26 -0400

    Test game of life logic

--- test/home/gol.js
+++ test/home/gol.js
@@ -1,5 +1,7 @@
 // Test the game of life script
 
+/* eslint-disable no-console */
+
 const test = require('ava');
 const gol = require('../../app/js/home/gol.js');
 
@@ -12,11 +14,47 @@ test('Properly initializes game of life', (t) => {
 
 test('Game of life boardSize works', (t) => {
   const g = new gol.Game(15, 15);
+  // Check basic boardSize
   t.deepEqual(g.boardSize, [15, 15]);
+  // Check that boardSize changes when a new row is added
   g.board.push(new Array(15).fill(false));
   t.deepEqual(g.boardSize, [16, 15]);
+  // Check that boardSize changes when a new column is added
   for (let i = 0; i < g.boardSize[0]; i += 1) {
     g.board[i].push(false);
   }
   t.deepEqual(g.boardSize, [16, 16]);
+});
+
+
+test('Game of life logic works', (t) => {
+  // Test game of life with a glider
+  const g = new gol.Game(10, 10);
+  g.clear();
+  // Create a glider
+  g.turnOn([
+            [2, 1],
+                    [3, 2],
+    [1, 3], [2, 3], [3, 3],
+  ]);
+  for (let i = 0; i < 10; i += 1) {
+    g.print();
+    g.step();
+    console.log();
+  }
+  t.deepEqual(
+    g.board,
+    [
+      [false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, true, false, false, false, false],
+      [false, false, false, false, false, false, true, false, false, false],
+      [false, false, false, false, true, true, true, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false],
+    ]
+  );
 });
