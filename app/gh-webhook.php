28bb11c695f502579d1fac8b052acc8ec631e82a
Luke Deen Taylor <luke@deentaylor.com>
2016-12-04 13:44:02 -0500

    Add debug stuff to webhook script

--- app/gh-webhook.php
+++ app/gh-webhook.php
@@ -1,11 +1,11 @@
 <?php
 
 if ($_POST['payload']) {
+  file_put_contents('webhook-test.json', $_POST['payload']);
   shell_exec('cd /var/www/controversial.io');
   shell_exec('git pull');
   shell_exec('npm install');
   shell_exec('gulp build');
-  shell_exec('echo hello > webhook-test.txt');
 }
 
 ?>
