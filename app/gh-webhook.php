d8a2f49272a4b87cd86408937ebf2e2e09b44ddc
Luke Deen Taylor <luke@deentaylor.com>
2016-12-04 15:18:27 -0500

    Fix webhook (I think)

--- app/gh-webhook.php
+++ app/gh-webhook.php
@@ -1,11 +1,14 @@
 <?php
 
-if ($_POST['payload']) {
-  file_put_contents('webhook-test.json', $_POST['payload']);
+$payload = json_decode(file_get_contents('php://input'));
+
+if ($payload) {
+  file_put_contents('/var/www/controversial.io/webhook-test.json', file_get_contents('php://input'));
   shell_exec('cd /var/www/controversial.io');
   shell_exec('git pull');
   shell_exec('npm install');
   shell_exec('gulp build');
+  echo('done');
 }
 
 ?>
