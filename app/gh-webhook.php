38f0a0ead13d9e4b7b604249bbc44cb0ec5c223c
Luke Deen Taylor <luke@deentaylor.com>
2016-12-04 15:21:46 -0500

    Use 2>&1 for enhanced debugging

--- app/gh-webhook.php
+++ app/gh-webhook.php
@@ -3,11 +3,10 @@
 $payload = json_decode(file_get_contents('php://input'));
 
 if ($payload) {
-  file_put_contents('/var/www/controversial.io/webhook-test.json', file_get_contents('php://input'));
-  shell_exec('cd /var/www/controversial.io');
-  shell_exec('git pull');
-  shell_exec('npm install');
-  shell_exec('gulp build');
+  echo(shell_exec('cd /var/www/controversial.io 2>&1'));
+  echo(shell_exec('git pull 2>&1'));
+  echo(shell_exec('npm install 2>&1'));
+  echo(shell_exec('gulp build 2>&1'));
   echo('done');
 }
 
