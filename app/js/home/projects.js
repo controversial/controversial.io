5d00e5b0f9996c0aa6712a3b334e89113d311e26
Luke Taylor <luke@deentaylor.com>
2017-07-18 15:12:04 -0400

    Laptop class

--- app/js/home/projects.js
+++ app/js/home/projects.js
@@ -1,32 +1,47 @@
 // Convert an element into a 3D laptop, moving its contents into the screen.
 
-function laptopify(elem, index) {
-  elem.classList.add('laptop3d');
-  elem.setAttribute('data-laptop3d-index', index);
-  const children = [...elem.childNodes];
-  elem.innerHTML = `
-    <div class="base"></div>
-    <div class="lid">
-      <div class="front">
-        <div class="screen"></div>
-      </div>
-      <div class="back"></div>
-    </div>
-  `;
-  const screen = elem.getElementsByClassName('screen')[0];
-  for (let i = 0; i < children.length; i += 1) {
-    screen.appendChild(children[i]);
-  }
-  // Add offset (translateX)
-  elem.style.transform = `${getComputedStyle(elem).transform} translateX(${30 * index}vw)`;
 
-  // Lift up a bit on hover
-  elem.addEventListener('mouseenter', () => {
-    elem.style.transform += ' translateZ(1vw)';
-  });
-  elem.addEventListener('mouseleave', () => {
-    elem.style.transform = elem.style.transform.replace(' translateZ(1vw)', '');
-  });
+class Laptop {
+  constructor(elem, index, dummy = false) {
+    this.elem = elem;
+    this.index = index;
+    this.dummy = dummy;
+
+    this.elem.classList.add('laptop3d');
+    if (this.dummy) this.elem.classList.add('dummy');
+    if (this.index) this.elem.setAttribute('data-laptop3d-index', this.index);
+
+    const children = [...this.elem.childNodes];
+    this.elem.innerHTML = `
+      <div class="base"></div>
+      <div class="lid">
+        <div class="front">
+          <div class="screen"></div>
+        </div>
+        <div class="back"></div>
+      </div>
+    `;
+    // Record some common elements as properties
+    ['base', 'lid', 'screen'].forEach((name) => {
+      this[name] = this.elem.getElementsByClassName(name)[0];
+    });
+    // Add all former children to the new screen element
+    children.forEach(child => this.screen.appendChild(child));
+
+    // Transform
+    console.log(`${getComputedStyle(this.elem).transform} translateX(${30 * this.index})vw`);
+    this.elem.style.transform = `${getComputedStyle(this.elem).transform} translateX(${30 * this.index}vw)`;
+
+
+    // Lift up a bit on hover
+    this.elem.addEventListener('mouseenter', () => {
+      elem.style.transform += ' translateZ(1vw)';
+    });
+
+    this.elem.addEventListener('mouseleave', () => {
+      elem.style.transform = elem.style.transform.replace(' translateZ(1vw)', '');
+    });
+  }
 }
 
 
@@ -34,15 +49,12 @@ function laptopify(elem, index) {
 document.addEventListener('DOMContentLoaded', () => {
   // Convert all elements with the class 'laptop3d' into laptops
   const laptopsContainer = document.getElementsByClassName('laptops-container')[0];
-  const laptops = laptopsContainer.getElementsByClassName('laptop3d');
-  for (let i = 0; i < laptops.length; i += 1) {
-    laptopify(laptops[i], i);
-  }
-  // Add dummy laptops
-  for (let i = 1; i <= 10; i += 1) {
-    const e = document.createElement('div');
-    e.classList.add('dummy');
-    laptopsContainer.appendChild(e);
-    laptopify(e, -i);
-  }
+  const laptopElems = [...laptopsContainer.getElementsByClassName('laptop3d')];
+  const laptops = laptopElems.map((l, i) => new Laptop(l, i));
+  // Add 10 dummy laptops to the beginning
+  const dummyLaptops = new Array(10)
+    .fill(0)
+    .map(() => document.createElement('div'))
+    .map(l => laptopsContainer.appendChild(l))
+    .map((l, i) => new Laptop(l, -i - 1, true));
 });
