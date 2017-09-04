c89c7c772fd035bcdc4eadb8bab43a57cc11b0fe
Luke Taylor <luke@deentaylor.com>
2017-09-04 14:59:57 -0400

    Assign an index of -1 to the header laptop

--- app/js/home/init.js
+++ app/js/home/init.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   window.setupGol();
   window.setupLaptops();
-  window.headerLaptop = new window.Laptop(document.getElementById('header-wrapper').getElementsByClassName('laptop3d')[0]);
+  window.headerLaptop = new window.Laptop(document.getElementById('header-wrapper').getElementsByClassName('laptop3d')[0], -1);
 
   window.onscroll();
 });
