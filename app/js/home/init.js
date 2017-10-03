0073a61c6cefa3dcd06a13d04157f0d63a58846a
Luke Taylor <luke@deentaylor.com>
2017-10-03 16:00:31 +0000

    Scroll when down indicator clicked

--- app/js/home/init.js
+++ app/js/home/init.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   window.setupGol();
   window.setupLaptops();
+  // Initialize the top laptop
   window.headerLaptop = new window.Laptop(document.getElementById('header-wrapper').getElementsByClassName('laptop3d')[0], -1);
 
   window.onscroll();
 
   setTimeout(window.browserCheck, 1000);
+
+  // Configure scrolling when down indicator clicked
+  document.getElementsByClassName('down-indicator')[0].addEventListener('click', () => {
+    window.scrollTo({ top: window.innerHeight / 2, behavior: 'smooth' });
+  });
 });
