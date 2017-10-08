304337ac7f253fbb8865cbd16496032cc9b51ddd
Luke Taylor <luke@deentaylor.com>
2017-10-08 18:01:45 -0400

    display: none when browser support popup isn't shown

--- app/js/home/browsercheck.js
+++ app/js/home/browsercheck.js
@@ -18,16 +18,17 @@ Object.defineProperty(window, 'isFirefox', {
 
 function browserCheck() {
   if (!window.isChromium) {
-    if (window.isSafari || window.isFirefox) {
-      const popup = document.getElementById('browser-partially-supported');
-      // Show gentle popup
-      popup.classList.add('visible');
-      // Enable dismiss button
-      popup.getElementsByTagName('button')[0].addEventListener('click', () => popup.classList.remove('visible'));
-    } else {
-      // Show serious popup
-      document.getElementById('browser-unsupported').classList.add('visible');
+    const willWork = window.isSafari || window.isFirefox;
+    const popup = document.getElementById(willWork ? 'browser-partially-supported' : 'browser-unsupported');
+    if (willWork) {
+      popup.getElementsByTagName('button')[0].addEventListener('click', () => {
+        popup.style.transform = 'translateY(100%)';
+        setTimeout(() => { popup.style.display = 'none'; }, 400);
+      });
     }
+    // Show popup
+    popup.style.display = 'block';
+    setTimeout(() => popup.classList.add('visible'));
   }
 }
 window.browserCheck = browserCheck;
