449af2260dc8dd4268c4585eb98150cdb770a6fb
Luke Taylor <luke@deentaylor.com>
2017-10-20 20:22:16 +0000

    Clean up browser check

--- app/js/home/browsercheck.js
+++ app/js/home/browsercheck.js
@@ -19,16 +19,18 @@ Object.defineProperty(window, 'isFirefox', {
 function browserCheck() {
   if (!window.isChromium) {
     const willWork = window.isSafari || window.isFirefox;
+    // Different popups if the browser is partially supported or entirely unsupported
     const popup = document.getElementById(willWork ? 'browser-partially-supported' : 'browser-unsupported');
+    // Only the "partially supported" popup can be dismissed
     if (willWork) {
       popup.getElementsByTagName('button')[0].addEventListener('click', () => {
         popup.style.transform = 'translateY(100%)';
+        // Wait for animation to finish before derendering
         setTimeout(() => { popup.style.display = 'none'; }, 400);
       });
     }
-    // Show popup
+    // Show popup (animation will automatically play when element is displayed)
     popup.style.display = 'block';
-    setTimeout(() => popup.classList.add('visible'));
   }
 }
 window.browserCheck = browserCheck;
