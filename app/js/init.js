f4985c3b6a55f316f3818ff821ee84718b715459
Luke Taylor <luke@deentaylor.com>
2017-10-25 00:43:07 -0400

    Allow going back up by clicking navigation toggle again

--- app/js/init.js
+++ app/js/init.js
@@ -9,6 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Configure scrolling when navigation trigger clicked
   document.getElementById('navigation-trigger').addEventListener('click', () => {
-    window.scrollTo({ top: window.innerHeight / 2, behavior: 'smooth' });
+    // Scroll to top if user is scrolled more than 10 pixels, otherwise scroll to open navigation
+    const scrollDest = window.scrollY < 10 ? window.innerHeight / 2 : 0;
+    window.scrollTo({ top: scrollDest, behavior: 'smooth' });
   });
 });
