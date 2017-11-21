ec2042ae2a1b35631bea47763013127477bbd5b5
Luke Taylor <luke@deentaylor.com>
2017-11-21 12:17:08 -0500

    Move init into Navigation constructor and Navigation instantiation into init

--- app/main/navigation/index.js
+++ app/main/navigation/index.js
@@ -16,13 +16,37 @@ class Navigation {
     this.pages = Array.from(pages);
 
     this.shown = false;
-
     this.navTransitionDuration = 300;
+
+    this.navBarUpdate();
+    window.addEventListener('hashchange', Navigation.navBarUpdate);
+
+    this.transitionUpdate(0);
+    // Jump to either open or closed on resize
+    window.addEventListener('resize', () => this.transitionUpdate(this.shown)); // this.shown is 1 if shown, 0 if not
+
+    // Set up laptops
+    // Convert all elements with the class 'laptop3d' into laptops
+    const laptopsContainer = document.getElementsByClassName('laptops-container')[0];
+    const laptopElems = [...laptopsContainer.getElementsByClassName('laptop3d')];
+    const laptops = laptopElems.map((l, i) => new Laptop(l, i - 1));
+    // Create laptop carousel
+    this.carousel = new LaptopCarousel(laptopsContainer, laptops);
+
+    // Configure navigation trigger clicked
+    document.getElementById('navigation-trigger').addEventListener('click', () => this.toggle());
+  }
+
+
+  static get hash() {
+    return window.location.hash.startsWith('#')
+      ? window.location.hash.substring(1)
+      : window.location.hash;
   }
 
 
   /** Switch to a new page */
-  async navigate(hash) {
+  async navigateTo(hash) {
     const laptop = this.carousel.getLaptopWithHash(hash);
 
     await this.open();
@@ -55,7 +79,6 @@ class Navigation {
     });
   }
 
-
   /** Hide navigation */
   close() {
     return new Promise((resolve) => {
@@ -81,61 +104,29 @@ class Navigation {
     });
   }
 
-
   /** Toggle navigation */
   toggle() {
     if (this.open) return this.close();
     return this.open();
   }
 
-}
 
-window.nav = new Navigation(
-  window.carousel,
-  new HomeAnim(),
-  new AboutAnim(),
-  new WorkAnim()
-);
+  static navBarUpdate() {
+    const linkToHere = document.getElementById('navigation').querySelector(`a[href='#${Navigation.hash}']`);
+    // un-highlight all the links
+    document.getElementById('navigation').querySelectorAll('a').forEach(a => a.classList.remove('selected'));
+    // re-highlight the right one
+    linkToHere.classList.add('selected');
+  }
 
-
-//
-
-
-export async function navigate(hash, animated = true) {
-  const laptop = window.carousel.getLaptopWithHash(hash);
-  if (!window.navShown) await navToggle();
-  laptop.centerSelf();
-}
-
-export function navBarUpdate(initial = false) {
-  const hash = window.location.hash.startsWith('#')
-                 ? window.location.hash.substring(1)
-                 : window.location.hash;
-
-  const linkToHere = document.getElementById('navigation').querySelector(`a[href='#${hash}']`);
-
-  document.getElementById('navigation').querySelectorAll('a').forEach(a => a.classList.remove('selected'));
-  linkToHere.classList.add('selected');
-
-  // No navigation animation necessary on page load
-  if (initial !== true) navigate(hash);
 }
 
 
 export default function init() {
-  navBarUpdate(true);
-  window.addEventListener('hashchange', navBarUpdate);
-  navTransitionUpdate(0);
-  window.addEventListener('resize', () => navTransitionUpdate(window.navShown ? 1 : 0));
-
-  // Set up laptops
-  // Convert all elements with the class 'laptop3d' into laptops
-  const laptopsContainer = document.getElementsByClassName('laptops-container')[0];
-  const laptopElems = [...laptopsContainer.getElementsByClassName('laptop3d')];
-  const laptops = laptopElems.map((l, i) => new Laptop(l, i - 1));
-  // Create laptop carousel
-  window.carousel = new LaptopCarousel(laptopsContainer, laptops);
-
-  // Configure navigation trigger clicked
-  document.getElementById('navigation-trigger').addEventListener('click', navToggle);
+  window.nav = window.navigation = new Navigation(
+    window.carousel,
+    new HomeAnim(),
+    new AboutAnim(),
+    new WorkAnim()
+  );
 }
