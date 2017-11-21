88ec0e78789328f9a1a678f4454888ee82a0ce75
Luke Taylor <luke@deentaylor.com>
2017-11-21 13:21:39 -0500

    Put debug messages in AboutNavigationAnimation and WorkNavigationAnimation

--- app/main/about/nav-shrink.js
+++ app/main/about/nav-shrink.js
@@ -1,3 +1,18 @@
 import NavigationBase from '../navigation/anim-base';
 
-export default class AboutNavigationAnimation extends NavigationBase {}
+export default class AboutNavigationAnimation extends NavigationBase {
+  scale(laptopScreenCoordinates, progress) {
+    super.scale(laptopScreenCoordinates, progress);
+    console.log(`About scaled to ${progress}`);
+  }
+
+  putInLaptop() {
+    super.putInLaptop();
+    console.log('About in laptop');
+  }
+
+  removeFromLaptop() {
+    super.putInLaptop();
+    console.log('About removed from laptop');
+  }
+}
--- app/main/work/nav-shrink.js
+++ app/main/work/nav-shrink.js
@@ -1,3 +1,18 @@
 import NavigationBase from '../navigation/anim-base';
 
-export default class WorkNavigationAnimation extends NavigationBase {}
+export default class WorkNavigationAnimation extends NavigationBase {
+  scale(laptopScreenCoordinates, progress) {
+    super.scale(laptopScreenCoordinates, progress);
+    console.log(`Work scaled to ${progress}`);
+  }
+
+  putInLaptop() {
+    super.putInLaptop();
+    console.log('Work in laptop');
+  }
+
+  removeFromLaptop() {
+    super.putInLaptop();
+    console.log('Work removed from laptop');
+  }
+}
