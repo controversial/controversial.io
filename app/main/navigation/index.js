2c79fbc401e47ee3fec55ae798de6caaed0158ae
Luke Taylor <luke@deentaylor.com>
2017-11-21 11:34:49 -0500

    Begin more organized Navigation class

--- app/main/navigation/index.js
+++ app/main/navigation/index.js
@@ -1,6 +1,106 @@
 import { Laptop, LaptopCarousel } from './laptops';
 import { update as navTransitionUpdate, navToggle } from './anim';
 
+// Subclasses of NavigationAnimationBase
+import HomeAnim from '../home/nav-shrink';
+import AboutAnim from '../about/nav-shrink';
+import WorkAnim from '../work/nav-shrink';
+
+const os = window.ease.outSin;
+
+
+/** Coordinates navigation */
+class Navigation {
+  constructor(carousel, ...pages) {
+    this.carousel = carousel;
+    this.pages = Array.from(pages);
+
+    this.shown = false;
+
+    this.navTransitionDuration = 300;
+  }
+
+
+  /** Switch to a new page */
+  async navigate(hash) {
+    const laptop = this.carousel.getLaptopWithHash(hash);
+
+    await this.open();
+    await laptop.centerSelf();
+  }
+
+
+  /** Show navigation */
+  open() {
+    return new Promise((resolve) => {
+      // Only needs to happen if it's not already open
+      if (!this.open) {
+        // Set up each animation frame for its correct time
+        for (let i = 0; i < 1; i += (1 / 60)) {
+          setTimeout(
+            () => this.update(os(i)),       // Update to proper progress
+            i * this.navTransitionDuration  // at proper time
+          );
+        }
+        // When we're finished
+        setTimeout(
+          () => { this.update(1); resolve(); },
+          this.navTransitionDuration
+        );
+        window.navShown = true;
+      // If it's already open, resolve immediately
+      } else {
+        resolve();
+      }
+    });
+  }
+
+
+  /** Hide navigation */
+  close() {
+    return new Promise((resolve) => {
+      // Only needs to happen if it's not already open
+      if (!this.open) {
+        // Set up each animation frame for its correct time
+        for (let i = 0; i < 1; i += (1 / 60)) {
+          setTimeout(
+            () => this.update(1 - os(i)),   // Update to proper progress
+            i * this.navTransitionDuration  // at proper time
+          );
+        }
+        // When we're finished
+        setTimeout(
+          () => { this.update(0); resolve(); },
+          this.navTransitionDuration
+        );
+        window.navShown = false;
+      // If it's already open, resolve immediately
+      } else {
+        resolve();
+      }
+    });
+  }
+
+
+  /** Toggle navigation */
+  toggle() {
+    if (this.open) return this.close();
+    return this.open();
+  }
+
+}
+
+window.nav = new Navigation(
+  window.carousel,
+  new HomeAnim(),
+  new AboutAnim(),
+  new WorkAnim()
+);
+
+
+//
+
+
 export async function navigate(hash, animated = true) {
   const laptop = window.carousel.getLaptopWithHash(hash);
   if (!window.navShown) await navToggle();
