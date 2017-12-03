492ae01fe01aa29462b707420f9dc0988807c21f
Luke Taylor <luke@deentaylor.com>
2017-12-03 15:20:55 -0500

    Set up AgeRenderer

--- app/main/about/index.js
+++ app/main/about/index.js
@@ -3,5 +3,5 @@ import Parallax3D from './parallax';
 
 export default function init() {
   window.parallax = new Parallax3D();
-  console.log(new AgeRenderer(undefined, '11/03/2001 17:23').getYears());
+  new AgeRenderer(document.getElementById('age'), '11/03/2001 17:23').loop();
 }
