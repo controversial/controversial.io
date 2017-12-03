b9ade43671684e6344cbfcc00b3efe29098cf169
Luke Taylor <luke@deentaylor.com>
2017-12-03 16:16:10 -0500

    Improve about page scaling

--- app/main/about/nav-shrink.js
+++ app/main/about/nav-shrink.js
@@ -8,16 +8,34 @@ export default class AboutNavigationAnimation extends NavigationBase {
     Object.assign(this.elem, {
       wrapper: document.getElementById('about-wrapper'),
       container: document.querySelector('#about-wrapper .container'),
+      info: document.querySelector('#about-wrapper .info'),
+      content: document.querySelector('#about-wrapper .content'),
     });
+
+    this.fontSize = {
+      content: 'calc(13px + .75vw)',
+      info: '4vw',
+    };
   }
 
 
   putInLaptop() {
     super.putInLaptop();
+    // Calculate appropriate font size scale
+    this.scaleX = this.elem.container.getBoundingClientRect().width / window.innerWidth;
+    // Scale font sizes using CSS `calc`
+    Object.entries(this.fontSize).forEach((pair) => {
+      const [key, value] = pair;
+      this.elem[key].style.fontSize = `calc(${value} * ${this.scaleX})`;
+    });
   }
 
 
   removeFromLaptop() {
     super.removeFromLaptop();
+    // Remove adjusted font sizes
+    Array.from(Object.keys(this.fontSize)).forEach((n) => {
+      this.elem[n].style.fontSize = '';
+    });
   }
 }
