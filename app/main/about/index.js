b59a6d6c670932d0bdb39e6efd26317b0515715a
Luke Taylor <luke@deentaylor.com>
2017-12-03 21:48:35 -0500

    Set up Parallax3D

--- app/main/about/index.js
+++ app/main/about/index.js
@@ -2,6 +2,11 @@ import AgeRenderer from './age';
 import Parallax3D from './parallax';
 
 export default function init() {
-  window.parallax = new Parallax3D();
+  window.parallax = new Parallax3D(
+    document
+      .getElementById('about-wrapper')
+      .getElementsByClassName('info')[0],
+    'translate(-50%, -50%)',
+  );
   new AgeRenderer(document.getElementById('age'), '11/03/2001 17:23').loop();
 }
