7a925cd353069cce9c7460c75cf613d0b892397e
Luke Taylor <luke@deentaylor.com>
2017-12-04 15:06:52 -0500

    Store mouse coordinates in Parallax3D
    Allows calling rotate() without knowing mouse coordinates

--- app/main/about/parallax.js
+++ app/main/about/parallax.js
@@ -5,44 +5,39 @@ export default class Parallax3D {
     this.initialTransform = initialTransform;
     this.rotmax = 10;
 
-    window.addEventListener('mousemove', e => this.update(e));
+    this.mousePos = { x: 0, y: 0 };
+    window.addEventListener('mousemove', (e) => { this.mouse(e); this.rotate(); });
   }
 
 
-  /** Get mouse coordinates on the screen in range [-1, 1] */
-  static getMouse(e) {
+  mouse(e) {
     const w = window.innerWidth; const h = window.innerHeight;
     const halfw = w / 2; const halfh = h / 2;
     const x = e.clientX; const y = e.clientY;
     const finalx = (x - halfw) / halfw; const finaly = (y - halfh) / halfh;
-    return [finalx, finaly];
+    [this.mousePos.x, this.mousePos.y] = [finalx, finaly];
   }
 
 
   /** Adjust rotation based on values from -1 to 1 */
-  rotate(rotx, roty) {
-    this.elem.style.transform = [
-      this.initialTransform,
-      `rotateX(${rotx * this.rotmax}deg)`,
-      `rotateY(${roty * this.rotmax}deg)`,
-    ].join(' ');
-  }
-
-
-  /** Update rotation from a mouse event */
-  update(e) {
+  rotate() {
     if (!this.disabled) {
-      // Flip mouse position and rotate by that amount
-      this.rotate(...Parallax3D.getMouse(e).map(n => -n).reverse());
+      this.elem.style.transform = [
+        this.initialTransform,
+        `rotateX(${-this.mousePos.y * this.rotmax}deg)`,
+        `rotateY(${-this.mousePos.x * this.rotmax}deg)`,
+      ].join(' ');
     }
   }
 
 
   disable() {
+    this.mousePos = { x: 0, y: 0 };
+    this.rotate();
     this.disabled = true;
-    this.rotate(0, 0);
   }
   enable() {
     this.disabled = false;
+    this.rotate();
   }
 }
