a2f76a5a197660ebdd4a3734a3928b7c33c53b52
Luke Taylor <luke@deentaylor.com>
2017-12-09 12:48:13 -0500

    Change border width of About page with scale

--- app/main/about/nav-shrink.js
+++ app/main/about/nav-shrink.js
@@ -16,6 +16,7 @@ export default class AboutNavigationAnimation extends NavigationBase {
       container: document.querySelector('#about-wrapper .container'),
       monogram: document.querySelector('#about-wrapper .monogram'),
       content: document.querySelector('#about-wrapper .content'),
+      border: document.querySelector('#about-wrapper .border'),
     });
 
     this.fontSize = {
@@ -43,6 +44,9 @@ export default class AboutNavigationAnimation extends NavigationBase {
       const [key, value] = pair;
       this.elem[key].style.fontSize = `calc(${value} * ${scaleX})`;
     });
+    // Adjust border width so that it doesn't jump to thicker when 'transform: scale' is removed
+    // Has a lower bound of 1px to ensure border still renders
+    this.elem.border.style.borderWidth = `${Math.max(1, 2 * scaleX)}px`;
     // Turn off parallax effect
     if (window.parallax) window.parallax.disable();
   }
@@ -54,6 +58,8 @@ export default class AboutNavigationAnimation extends NavigationBase {
     Array.from(Object.keys(this.fontSize)).forEach((n) => {
       this.elem[n].style.fontSize = '';
     });
+    // Remove adjusted border width
+    this.elem.border.style.borderWidth = '';
     // Re-enable parallax effect
     if (window.parallax) window.parallax.enable();
   }
