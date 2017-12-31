7c5e6f1b3713c454465631fe97eb8fbd65e38b16
Luke Taylor <luke@deentaylor.com>
2017-12-31 10:49:35 -0500

    Remove old about page scaling behavior

--- app/main/about/nav-shrink.js
+++ app/main/about/nav-shrink.js
@@ -14,15 +14,8 @@ export default class AboutNavigationAnimation extends NavigationBase {
     Object.assign(this.elem, {
       wrapper: document.getElementById('about-wrapper'),
       background: document.querySelector('#about-wrapper .background'),
-      monogram: document.querySelector('#about-wrapper .monogram'),
-      content: document.querySelector('#about-wrapper .content'),
-      border: document.querySelector('#about-wrapper .border'),
+      info: document.querySelector('#about-wrapper .info'),
     });
-
-    this.fontSize = {
-      content: 'calc(13px + .75vw)',
-      monogram: '4vw',
-    };
   }
 
 
@@ -37,16 +30,7 @@ export default class AboutNavigationAnimation extends NavigationBase {
 
   putInLaptop() {
     super.putInLaptop();
-    // Calculate appropriate font size scale
-    const scaleX = this.elem.background.getBoundingClientRect().width / window.innerWidth;
-    // Scale font sizes using CSS `calc`
-    Object.entries(this.fontSize).forEach((pair) => {
-      const [key, value] = pair;
-      this.elem[key].style.fontSize = `calc(${value} * ${scaleX})`;
-    });
-    // Adjust border width so that it doesn't jump to thicker when 'transform: scale' is removed
-    // Has a lower bound of 1px to ensure border still renders
-    this.elem.border.style.borderWidth = `${Math.max(3 * scaleX, 1)}px`;
+
     // Turn off parallax effect
     if (window.parallax) window.parallax.disable();
   }
@@ -54,12 +38,7 @@ export default class AboutNavigationAnimation extends NavigationBase {
 
   removeFromLaptop() {
     super.removeFromLaptop();
-    // Remove adjusted font sizes
-    Array.from(Object.keys(this.fontSize)).forEach((n) => {
-      this.elem[n].style.fontSize = '';
-    });
-    // Remove adjusted border width
-    this.elem.border.style.borderWidth = '';
+
     // Re-enable parallax effect
     if (window.parallax) window.parallax.enable();
   }
