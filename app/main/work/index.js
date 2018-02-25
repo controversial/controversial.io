482e2e6328107f5a0acb50b245793f1158dcf5fd
Luke Taylor <luke@deentaylor.com>
2018-02-25 11:08:45 -0500

    Initialize work Carousel

--- app/main/work/index.js
+++ app/main/work/index.js
@@ -1,2 +1,8 @@
+import { CarouselCard, Carousel } from './carousel';
+
 export default function init() {
+  const projectsContainer = document.getElementsByClassName('projects-container')[0];
+  const cardElems = [...projectsContainer.getElementsByClassName('carousel-card')];
+  const cards = cardElems.map((c, i) => new CarouselCard(c, i));
+  window.workCarousel = new Carousel(cards);
 }
