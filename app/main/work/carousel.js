64f2a34f3e2bdb9c657a123a8005029a07d8c5d4
Luke Taylor <luke@deentaylor.com>
2018-02-25 11:07:00 -0500

    Most basic scaffolding of work page carousel

--- app/main/work/carousel.js
+++ app/main/work/carousel.js
@@ -1,5 +1,21 @@
 /** Represents a single project in the work carousel */
-export class CarouselItem {}
+export class CarouselCard {
+  constructor(elem) {
+    this.elem = elem;
+    this.elem.classList.add('carousel-card');
+  }
+}
 
-/** Collects and orchestrates multiple CarouselItems */
-export class Carousel {}
+/** Collects and orchestrates multiple CarouselCards */
+export class Carousel {
+  constructor(cards) {
+    this.cards = cards;
+    this._position = 0;
+  }
+
+  get position() { return this._position; }
+  set position(pos) {
+    // Move
+    this._position = pos;
+  }
+}
