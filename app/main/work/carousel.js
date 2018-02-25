823da4f7aacc565100b455b0c8aead86808cdfab
Luke Taylor <luke@deentaylor.com>
2018-02-25 13:05:33 -0500

    Parallax on work cards

--- app/main/work/carousel.js
+++ app/main/work/carousel.js
@@ -1,10 +1,13 @@
 /** Represents a single project in the work carousel */
+import Parallax3D from '../parallax';
+
 export class CarouselCard {
   constructor(elem, tags) {
     this.elem = elem;
+    this.elem.classList.add('carousel-card');
     this.tags = tags || elem.dataset.tags.split(',').map(t => t.trim());
 
-    this.elem.classList.add('carousel-card');
+    this.parallax = new Parallax3D(this.elem, 'translate(-50%, -50%)');
   }
 }
 
