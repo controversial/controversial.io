c5d2f43ef63f5da18950c40d6abc937ab6b75250
Luke Taylor <luke@deentaylor.com>
2018-02-28 12:50:17 -0500

    Correctly move carousel into laptop when shrinking

--- app/main/work/nav-shrink.js
+++ app/main/work/nav-shrink.js
@@ -15,6 +15,7 @@ export default class WorkNavigationAnimation extends NavigationBase {
       wrapper: document.getElementById('work-wrapper'),
       background: document.querySelector('#work-wrapper .background'),
       tags: document.querySelector('#work-wrapper .tags'),
+      carousel: document.querySelector('#work-wrapper .projects-container'),
     });
   }
 
@@ -22,17 +23,23 @@ export default class WorkNavigationAnimation extends NavigationBase {
     const scaleInfo = super.scale(laptopScreenCoordinates, progress);
     const [, scaleNeeded] = scaleInfo;
 
-    const tagsScale = laptopScreenCoordinates.width / window.innerWidth;
-    const targetTop = laptopScreenCoordinates.top + (scaleNeeded.y * this.elem.tags.offsetTop);
-    const tagsTranslate = targetTop - this.elem.tags.offsetTop;
+    const targetScale = laptopScreenCoordinates.width / window.innerWidth;
+    const scale = NavigationBase.tween(1, targetScale, progress);
 
-
-    const translate = NavigationBase.tween(0, tagsTranslate, progress);
-    const scale = NavigationBase.tween(1, tagsScale, progress);
+    const targetTagsTop = laptopScreenCoordinates.top + (scaleNeeded.y * this.elem.tags.offsetTop);
+    const targetTagsTranslate = targetTagsTop - this.elem.tags.offsetTop;
+    const tagsTranslate = NavigationBase.tween(0, targetTagsTranslate, progress);
     // Store as attributes for use later
-    this.elem.tags.dataset.translate = translate;
+    this.elem.tags.dataset.translate = tagsTranslate;
     this.elem.tags.dataset.scale = scale;
-    this.elem.tags.style.transform = `translateX(-50%) translateY(${translate}px) scale(${scale})`;
+    this.elem.tags.style.transform = `translateX(-50%) translateY(${tagsTranslate}px) scale(${scale})`;
+
+    const targetCarouselY = laptopScreenCoordinates.top + (laptopScreenCoordinates.height / 2);
+    const targetCarouselTranslate = targetCarouselY - (window.innerHeight / 2);
+    const carouselTranslate = NavigationBase.tween(0, targetCarouselTranslate, progress);
+    this.elem.carousel.dataset.translate = carouselTranslate;
+    this.elem.carousel.dataset.scale = scale;
+    this.elem.carousel.style.transform = `translateY(${carouselTranslate}px) scale(${scale})`;
   }
 
   putInLaptop() {
@@ -40,11 +47,19 @@ export default class WorkNavigationAnimation extends NavigationBase {
 
     this.elem.tags.style.transform = `translateX(-50%) scale(${this.elem.tags.dataset.scale})`;
     this.elem.laptopContent('work').appendChild(this.elem.tags);
+
+    this.elem.carousel.style.transform = `scale(${this.elem.carousel.dataset.scale})`;
+    this.elem.carousel.style.transformOrigin = '0 0';
+    this.elem.carousel.style.left = this.elem.background.style.left;
+    this.elem.laptopContent('work').appendChild(this.elem.carousel);
   }
 
   removeFromLaptop() {
     super.removeFromLaptop();
 
     this.elem.wrapper.appendChild(this.elem.tags);
+    this.elem.carousel.style.transformOrigin = 'initial';
+    this.elem.carousel.style.left = 0;
+    this.elem.wrapper.appendChild(this.elem.carousel);
   }
 }
