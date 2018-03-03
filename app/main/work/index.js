61b2d1eb81266c4f65b2f065ecd7623e66e52da1
Luke Taylor <luke@deentaylor.com>
2018-03-03 16:38:26 -0500

    Fix bad argument to CarouselCard construction

--- app/main/work/index.js
+++ app/main/work/index.js
@@ -3,6 +3,6 @@ import { CarouselCard, Carousel } from './carousel';
 export default function init() {
   const projectsContainer = document.getElementsByClassName('projects-container')[0];
   const cardElems = [...projectsContainer.getElementsByClassName('carousel-card')];
-  const cards = cardElems.map((c, i) => new CarouselCard(c, i));
+  const cards = cardElems.map(c => new CarouselCard(c));
   window.workCarousel = new Carousel(cards);
 }
