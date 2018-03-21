33dc05ab38db06a0dfed6ba3bd03b5787a3f43ae
Luke Taylor <luke@deentaylor.com>
2018-03-21 10:03:24 -0400

    Simplify gh-stars.js

--- app/main/work/gh-stars.js
+++ app/main/work/gh-stars.js
@@ -1,15 +1,13 @@
 export default async function fetchStars() {
-  // Repos from 'controversial' and 'arkis'
-  const controversialRepos = await fetch('https://api.github.com/users/controversial/repos?per_page=100')
-    .then(r => r.json());
-  const arkisRepos = await fetch('https://api.github.com/users/arkis/repos?per_page=100')
-    .then(r => r.json());
-  const repos = controversialRepos.concat(arkisRepos);
-
+  // Fetch repos from users 'controversial' and 'arkis'
+  const users = ['controversial', 'arkis'];
+  const requests = users
+    .map(user => `https://api.github.com/users/${user}/repos?per_page=100`)
+    .map(url => fetch(url).then(r => r.json()));
+  const repos = (await Promise.all(requests)).reduce((a, b) => a.concat(b));
+  // Process data into simple object
   const ghStars = {};
-  repos.forEach((r) => {
-    ghStars[r.full_name] = r.stargazers_count;
-  });
+  repos.forEach((r) => { ghStars[r.full_name] = r.stargazers_count; });
   window.ghStars = ghStars;
   return ghStars;
 }
